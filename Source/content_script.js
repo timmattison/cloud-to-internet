8adad50696fffc643e079931ee7c3b4304e6bb7e
Tim Mattison <tim@mattison.org>
2015-09-11 06:06:43 -0400

    Converted replacement text to "the Internet"

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bThe Cloud\b/g, "the Internet");
+	v = v.replace(/\bThe cloud\b/g, "the Internet");
+	v = v.replace(/\bthe Cloud\b/g, "the Internet");
+	v = v.replace(/\bthe cloud\b/g, "the Internet");
 	
 	textNode.nodeValue = v;
 }
